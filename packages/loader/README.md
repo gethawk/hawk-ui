f7c09a3952cd4f67f6efac73f14245ed819b7ae1
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-03-26 21:54:30 +0530

    add spinner loader

--- packages/loader/README.md
+++ packages/loader/README.md
@@ -2,7 +2,16 @@
 
 ```js
 <div className="styleguidist__btns-wrap">
-  <br />
+  <p>Jelly Loader</p>
+  <br /><br />
+  <div style={{ 'margin-left':'20px' }}>
+    <Loader
+      type="jelly"
+    />
+  </div>
+  <br /><br />
+  <p>Spinner Loader</p>
+  <br /><br />
   <div style={{ 'margin-left':'20px' }}>
     <Loader />
   </div>
--- packages/loader/src/index.js
+++ packages/loader/src/index.js
@@ -1,5 +1,5 @@
 // vendor modules
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import getClassnames from 'classnames';
 // css modules
@@ -10,19 +10,31 @@ import './index.scss';
  */
 export default class Loader extends Component {
   static propTypes = {
+    type: PropTypes.string,
     className: PropTypes.string,
   };
+  static defaultProps = {
+    type: 'spinner',
+  }
   state = {};
 
   render() {
-    const { className } = this.props;
+    const { type, className } = this.props;
 
     return (
-      <div className="hawk-loader">
-        <div className="hawk-loader__container">
-          <div className="hawk-loader__shadow" />
-          <div className={getClassnames('hawk-loader__box', className)} />
-        </div>
+      <div className={`hawk-loader__${type}`}>
+        {type === 'spinner' ? (
+          <Fragment>
+            <div className={`hawk-loader__${type}-bounce1`} />
+            <div className={`hawk-loader__${type}-bounce2`} />
+            <div className={`hawk-loader__${type}-bounce3`} />
+          </Fragment>
+        ) : (
+          <div className={`hawk-loader__${type}-container`}>
+            <div className={`hawk-loader__${type}-shadow`} />
+            <div className={getClassnames(`hawk-loader__${type}-box`, className)} />
+          </div>
+        )}
       </div>
     );
   }
