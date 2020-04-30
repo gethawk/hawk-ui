9dd8b96c1b9f238bb74cb0defc900e430f2f36ab
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-04-30 18:52:33 +0530

    update toast component

--- packages/toast/README.md
+++ packages/toast/README.md
@@ -2,21 +2,47 @@
 
 ```js
 initialState = {
-  isToast: true,
+  isShowWithout: false,
+  isShowWith: false,
 };
 
 <div className="styleguidist__btns-wrap">
+  <div>Without Overflow</div>
+  <br />
   <Toast
-    isVisible={state.isToast}
-  />
-  <button
-    type="button"
-    className="hawk-button"
-    onClick={() => {
-      setState({ isToast: state.isToast });
-    }}
+    isShow={state.isShowWithout}
+    position="top-left"
   >
-    Show Toast
-  </button>
+    <Toast.CONTENT
+      type="success"
+      title="title"
+      message="Success Message"
+      hideCloseOption
+      onClick={() => { setState({ isShowWithout: !state.isShowWithout }); }}
+    />
+  </Toast>
+  <Button
+    onClick={() => { setState({ isShowWithout: !state.isShowWithout }); }}
+  >Show Toast</Button>
+  <br /><br />
+  <div>With Overflow</div>
+  <br />
+  <Toast
+    isOverflow
+    isShow={state.isShowWith}
+    position="top-left"
+  >
+    <Toast.CONTENT
+      type="success"
+      title="title"
+      message="Success Message"
+      hideCloseOption
+      onClick={() => { setState({ isShowWith: !state.isShowWith }); }}
+    />
+    <Toast.OVERVIEW />
+  </Toast>
+  <Button
+    onClick={() => { setState({ isShowWith: !state.isShowWith }); }}
+  >Show Toast</Button>
 </div>
 ```
--- packages/toast/src/index.js
+++ packages/toast/src/index.js
@@ -6,59 +6,115 @@ import _ from 'lodash';
 // css modules
 import './index.scss';
 
+class ToastContent extends Component {
+  static displayName = 'ToastContent';
+  static propTypes = {
+    type: PropTypes.oneOf(['success', 'danger', 'info', 'warning']),
+    title: PropTypes.string,
+    message: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.string,
+    ]),
+    hideCloseOption: PropTypes.bool,
+    onClick: PropTypes.func,
+  };
+  static defaultProps = {
+    hideCloseOption: false,
+    type: 'success',
+  }
+  state = {};
+
+  onClick = () => {
+    if (this.props.hideCloseOption && _.isFunction(this.props.onClick)) {
+      this.props.onClick();
+    }
+  }
+
+  render() {
+    const { type, title, message, hideCloseOption } = this.props;
+
+    return (
+      <div
+        className={getClassnames('hawk-toast__content', {
+          [`hawk-toast__content-${type}`]: _.isString(type),
+        })}
+      >
+        <div className="hawk-toast__container">
+          <i
+            className={getClassnames('fa hawk-toast__container-icon', {
+              'fa-check-circle': _.isEqual(type, 'success'),
+              'fa-times-circle': _.isEqual(type, 'danger'),
+              'fa-info-circle': _.isEqual(type, 'info'),
+              'fa-exclamation-triangle': _.isEqual(type, 'warning'),
+            })}
+          />
+          <div className="hawk-toast__container-content">
+            {title && (
+              <div className="hawk-toast__container-content-title">{title}</div>
+            )}
+            {message && (
+              <div className="hawk-toast__container-content-message">{message}</div>
+            )}
+          </div>
+        </div>
+        {hideCloseOption && (
+          <i
+            className="fa fa-times hawk-toast__container-close"
+            onClick={this.onClick}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+class TableOverview extends Component {
+  static displayName = 'TableOverview';
+  state = {};
+
+  render() {
+    return (
+      <div className="hawk-toast__overflow" />
+    );
+  }
+}
+
 /**
  * @example ../README.md
  */
 export default class Toast extends Component {
+  static displayName = 'Toast';
   static propTypes = {
-    isVisible: PropTypes.bool,
-    isSuccess: PropTypes.bool,
-    isIcon: PropTypes.bool,
-    className: PropTypes.string,
+    children: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.array,
+      PropTypes.object,
+    ]),
     position: PropTypes.oneOf(['top-left', 'top-right', 'bottom-left', 'bottom-right']),
+    isShow: PropTypes.bool,
+    isOverflow: PropTypes.bool,
   };
   static defaultProps = {
-    isSuccess: true,
-    isIcon: true,
-    position: 'bottom-right',
+    position: 'top-right',
+    isShow: false,
+    isOverflow: false,
   }
-  state = {
-    isVisible: false,
-  };
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      isVisible: nextProps.isVisible,
-    });
-    setTimeout(() => {
-      this.setState({ isVisible: false });
-    }, 7000);
-  }
+  static CONTENT = ToastContent;
+  static OVERVIEW = TableOverview;
+  state = {};
 
   render() {
-    const { isSuccess, isIcon, className, position } = this.props;
+    const { children, position, isShow, isOverflow } = this.props;
 
     return (
       <div
         className={getClassnames('hawk-toast', {
+          'hawk-toast__hidden': !isShow,
           [`hawk-toast__${position}`]: _.isString(position),
+          [`hawk-toast__${position}-animation`]: !isOverflow,
         })}
       >
-        <div
-          className={getClassnames('hawk-toast__content', {
-            'hawk-toast__content-fadeIn': this.state.isVisible,
-            'hawk-toast__content-fadeOut': !this.state.isVisible,
-          })}
-        >
-          {isIcon ? (
-            <i
-              className={getClassnames('fa', className, {
-                'fa-check-circle': isSuccess,
-                'fa-times-circle': !isSuccess,
-              })}
-            />
-          ) : null}
-          <div className="hawk-toast__content-message">Heads up, toasts will stack automatically</div>
-        </div>
+        {children}
       </div>
     );
   }
