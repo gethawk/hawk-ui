db5cd974b420ee1a90a334f7faa5baec7fdafccd
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-07-05 19:29:03 +0530

    add block options

--- packages/email-editor/src/blocks/Blocks.js
+++ packages/email-editor/src/blocks/Blocks.js
@@ -1,29 +1,25 @@
 // vendor modules
 import React from 'react';
 // react modules
+import _ from 'lodash';
 import Card from '@hawk-ui/card';
+// utils modules
+import { blockOptions } from '../utils/blocks';
 
 export default function Blocks() {
   return (
     <div className="hawk-email-editor-blocks">
-      <Card
-        isClickable
-      />
-      <Card
-        isClickable
-      />
-      <Card
-        isClickable
-      />
-      <Card
-        isClickable
-      />
-      <Card
-        isClickable
-      />
-      <Card
-        isClickable
-      />
+      {_.map(blockOptions, (item, index) => (
+        <Card
+          key={index}
+          isClickable
+        >
+          <div className="hawk-email-editor-blocks__icon">
+            <i className={item.icon} />
+          </div>
+          <div className="hawk-email-editor-blocks__title">{item.title}</div>
+        </Card>
+      ))}
     </div>
   );
 }
---
+++ packages/email-editor/src/utils/blocks.js
@@ -0,0 +1,22 @@
+export const blockOptions = [
+  {
+    id: 1,
+    title: 'Text',
+    icon: 'fa fa-font',
+  },
+  {
+    id: 2,
+    title: 'Button',
+    icon: 'fa fa-square',
+  },
+  {
+    id: 3,
+    title: 'Divider',
+    icon: 'fa fa-square',
+  },
+  {
+    id: 4,
+    title: 'Image',
+    icon: 'fa fa-square',
+  },
+];
