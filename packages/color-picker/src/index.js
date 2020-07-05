707532907aacfdde6c1e209e9bb988c6374f5d2e
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-07-05 07:03:01 +0530

    modifed javascript

--- packages/color-picker/src/index.js
+++ packages/color-picker/src/index.js
@@ -1,7 +1,7 @@
 // vendor modules
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import getClassnames from 'classnames';
+// import PropTypes from 'prop-types';
+// import getClassnames from 'classnames';
 // css modules
 import './index.scss';
 
@@ -9,141 +9,131 @@ import './index.scss';
  * @example ../README.md
  */
 export default class ColorPicker extends Component {
-  state = {
-    prevX: 0,
-    prevY: 0,
-  };
+  state = {};
 
   componentDidMount() {
-    const colorBlock = document.getElementById('color-block');
-    const ctx1 = colorBlock.getContext('2d');
-    const width1 = colorBlock.width;
-    const height1 = colorBlock.height;
+    const inp1 = document.getElementById('inp1');
+    const inp2 = document.getElementById('inp2');
+    const inp3 = document.getElementById('inp3');
+    const txt = document.getElementById('txt');
+    const view = document.getElementById('view');
+    const root = document.documentElement;
+    let h = [];
+    let s = [];
+    let l = [];
 
-    const colorStrip = document.getElementById('color-strip');
-    const ctx2 = colorStrip.getContext('2d');
-    const width2 = colorStrip.width;
-    const height2 = colorStrip.height;
-
-    const colorLabel = document.getElementById('color-label');
-
-    let x = 0;
-    let y = 0;
-    let drag = false;
-    let rgbaColor = 'rgba(255,0,0,1)';
-
-    function fillGradient() {
-      ctx1.fillStyle = rgbaColor;
-      ctx1.fillRect(0, 0, width1, height1);
-
-      const grdWhite = ctx2.createLinearGradient(0, 0, width1, 0);
-
-      grdWhite.addColorStop(0, 'rgba(255,255,255,1)');
-      grdWhite.addColorStop(1, 'rgba(255,255,255,0)');
-      ctx1.fillStyle = grdWhite;
-      ctx1.fillRect(0, 0, width1, height1);
-
-      const grdBlack = ctx2.createLinearGradient(0, 0, 0, height1);
-
-      grdBlack.addColorStop(0, 'rgba(0,0,0,0)');
-      grdBlack.addColorStop(1, 'rgba(0,0,0,1)');
-      ctx1.fillStyle = grdBlack;
-      ctx1.fillRect(0, 0, width1, height1);
-    }
-
-    ctx1.rect(0, 0, width1, height1);
-    fillGradient();
-
-    ctx2.rect(0, 0, width2, height2);
-    const grd1 = ctx2.createLinearGradient(0, 0, 0, height1);
-
-    grd1.addColorStop(0, 'rgba(255, 0, 0, 1)');
-    grd1.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
-    grd1.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
-    grd1.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
-    grd1.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
-    grd1.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
-    grd1.addColorStop(1, 'rgba(255, 0, 0, 1)');
-    ctx2.fillStyle = grd1;
-    ctx2.fill();
-
-    function click(e) {
-      x = e.offsetX;
-      y = e.offsetY;
-      const imageData = ctx2.getImageData(x, y, 1, 1).data;
-
-      rgbaColor = `rgba(${imageData[0]},${imageData[1]},${imageData[2]},1)`;
-      fillGradient();
-    }
-
-    function drawCoordinates(x1, y1) {
-      const pointSize = 3;
-      const ctx = document.getElementById('color-block').getContext('2d');
-
-      ctx.fillStyle = '#ff2626';
-      // ctx.beginPath();
-      // ctx.clearRect(this.state.x1, this.state.y1, pointSize, 0, Math.PI * 2, true);
-      // ctx.closePath();
-      ctx.beginPath();
-      ctx.arc(x1, y1, pointSize, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.fill();
-      // this.setState({
-      //   prevX: x1,
-      //   prevY: y1,
-      // });
-    }
-
-    function changeColor(e) {
-      x = e.offsetX;
-      y = e.offsetY;
-      const imageData = ctx1.getImageData(x, y, 1, 1).data;
-
-      drawCoordinates(x, y);
-      rgbaColor = `rgba(${imageData[0]},${imageData[1]},${imageData[2]},1)`;
-      colorLabel.style.backgroundColor = rgbaColor;
-    }
-
-    function mousedown(e) {
-      drag = true;
-      changeColor(e);
-    }
-
-    function mousemove(e) {
-      if (drag) {
-        changeColor(e);
+    function update() {
+      h = [];
+      s = [];
+      l = [];
+      for (let i = 0; i < 360; i++) {
+        h.push(`hsl(${i + 1}, ${100}%, ${50}%)`);
       }
+      for (let i = 0; i < 100; i++) {
+        s.push(`hsl(${inp1.value}, ${i}%, 50%)`);
+        l.push(`hsl(${inp1.value}, 100%, ${i}%)`);
+      }
+      inp1.style.background = `linear-gradient(to right, ${h.join(', ')})`;
+      inp2.style.background = `linear-gradient(to right, ${s.join(', ')})`;
+      inp3.style.background = `linear-gradient(to right, ${l.join(', ')})`;
+
+      txt.value = `hsl(${inp1.value}, ${inp2.value}%, ${inp3.value}%)`;
+      view.style.backgroundColor = `hsl(${inp1.value}, ${inp2.value}%, ${inp3.value}%)`;
+
+      root.style.setProperty('--color1', `hsl(${inp1.value}, 100%, 50%)`);
+      root.style.setProperty('--color2', `hsl(${inp1.value}, ${inp2.value}%, 50%)`);
+      root.style.setProperty('--color3', `hsl(${inp1.value}, 100%, ${inp3.value}%)`);
+      document.getElementById('rgb').innerHTML = window.getComputedStyle(view).backgroundColor;
+
+      let str = window.getComputedStyle(view).backgroundColor;
+
+      str = str.replace('rgb', '');
+      str = str.replace('(', '');
+      str = str.replace(')', '');
+      str = str.split(',');
+      const hex = [0, 0, 0];
+
+      hex[0] = parseFloat(str[0]).toString(16);
+      hex[1] = parseFloat(str[1]).toString(16);
+      hex[2] = parseFloat(str[2]).toString(16);
+
+      if (hex[0].length < 2) {
+        hex[0] = `0${hex[0]}`;
+      }
+      if (hex[1].length < 2) {
+        hex[1] = `0${hex[1]}`;
+      }
+      if (hex[2].length < 2) {
+        hex[2] = `0${hex[2]}`;
+      }
+
+      document.getElementById('hex').innerHTML = `#${hex.join('')}`;
     }
+    update();
+    inp1.oninput = update;
+    inp2.oninput = update;
+    inp3.oninput = update;
 
-    function mouseup() {
-      drag = false;
+    function convert() {
+      let str = this.value;
+
+      str = str.replace('hsl', '');
+      str = str.replace('(', '');
+      str = str.replace(')', '');
+      str = str.replace('%', '');
+      str = str.replace('%', '');
+      str = str.split(',');
+      inp1.value = parseFloat(str[0]);
+      inp2.value = parseFloat(str[1]);
+      inp3.value = parseFloat(str[2]);
+      update();
     }
-
-    colorStrip.addEventListener('click', click, false);
-
-    colorBlock.addEventListener('mousedown', mousedown, false);
-    colorBlock.addEventListener('mouseup', mouseup, false);
-    colorBlock.addEventListener('mousemove', mousemove, false);
+    txt.oninput = convert;
   }
 
   render() {
     return (
       <div className="hawk-color-picker">
-        <div
-          htmlFor="color-input"
-          id="color-label"
-          style={{ backgroundColor: 'red' }}
-          className="hawk-color-picker__label"
-        />
-        <input type="checkbox" id="color-input" checked />
-        <div className="hawk-color-picker__block">
-          <canvas
-            className="hawk-color-picker__block-color"
-            id="color-block"
-            width="150"
-            height="150"
+        <div className="hawk-color-picker__view" id="view" />
+        <div className="hawk-color-picker__colors" id="colors">
+          <input
+            type="text"
+            id="txt"
+            value=""
+            className="hawk-color-picker__colors-text"
           />
-          <canvas className="hawk-color-picker__block-strip" id="color-strip" />
+          <button id="copy" className="hawk-color-picker__colors-copy">
+            <i className="fa fa-copy" />
+          </button>
+          <input
+            type="range"
+            className="hawk-color-picker__colors-input hawk-color-picker__colors-input1"
+            id="inp1"
+            min="0"
+            max="360"
+            value="331"
+            draggable="false"
+          />
+          <input
+            type="range"
+            className="hawk-color-picker__colors-input hawk-color-picker__colors-input2"
+            id="inp2"
+            min="0"
+            max="100"
+            value="100"
+            draggable="false"
+          />
+          <input
+            type="range"
+            className="hawk-color-picker__colors-input hawk-color-picker__colors-input3"
+            id="inp3"
+            min="0"
+            max="100"
+            value="50"
+            draggable="false"
+          />
+          <p id="rgb">rgb(255, 255, 255)</p>
+          <p id="hex">#ffffff</p>
         </div>
       </div>
     );
