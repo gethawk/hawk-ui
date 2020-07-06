b3906b3a9c8e02ee0c8dfd7f6720988db15640e0
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-07-06 06:37:38 +0530

    add styles layout

---
+++ packages/email-editor/src/globals/ColorsPicker.js
@@ -0,0 +1,8 @@
+// vendor modules
+import React from 'react';
+
+export default function ColorsPicker() {
+  return (
+    <div>Color Picker</div>
+  );
+}
--- packages/email-editor/src/styles/Styles.js
+++ packages/email-editor/src/styles/Styles.js
@@ -1,10 +1,24 @@
 // vendor modules
-import React from 'react';
+import React, { useState } from 'react';
+// react modules
+import Collapse from '@hawk-ui/collapse';
+// utils modules
+import { styleOptions } from '../utils/styles';
+// css modules
+import '../styles.scss';
 
 export default function Styles() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className="hawk-email-editor-styles">
-      Styles
+      <Collapse
+        items={styleOptions}
+        activeItem={activeIndex}
+        setActiveItem={(event) => {
+          setActiveIndex(event);
+        }}
+      />
     </div>
   );
 }
