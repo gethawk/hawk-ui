e13d0233c72e9ae7fb7e935db54d4eeac0f7b559
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-07-07 14:27:57 +0530

    fixed button component

--- packages/email-editor/src/components/blocks/hawkButton/HawkButton.js
+++ packages/email-editor/src/components/blocks/hawkButton/HawkButton.js
@@ -3,22 +3,25 @@ import React from 'react';
 // react modules
 import PropTypes from 'prop-types';
 import Button from '@hawk-ui/button';
-// import { useNode } from '@craftjs/core';
+import { useNode } from '@craftjs/core';
 import HawkButtonSettings from './HawkButtonSettings';
 
 export default function HawkButton(props) {
-  const { size, variant, color, children } = props;
-  // const { connectors: { connect, drag } } = useNode();
+  const { size, variant, color, text } = props;
+  const { connectors: { connect, drag } } = useNode();
 
   return (
-    <Button
-      size={size}
-      variant={variant}
-      color={color}
-      // ref={(ref) => { connect(drag(ref)); }}
+    <div
+      ref={(ref) => { connect(drag(ref)); }}
     >
-      {children}
-    </Button>
+      <Button
+        size={size}
+        variant={variant}
+        color={color}
+      >
+        {text}
+      </Button>
+    </div>
   );
 }
 
@@ -38,8 +41,5 @@ HawkButton.propTypes = {
   size: PropTypes.number,
   variant: PropTypes.string,
   color: PropTypes.string,
-  children: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-  ]),
+  text: PropTypes.string,
 };
