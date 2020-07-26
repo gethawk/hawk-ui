37c1a67113c319e2196fc4839fa1dad475bde34c
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-07-26 07:38:28 +0530

    fixed select dropdown component

--- packages/form/src/fields/FormSelectDropdown.js
+++ packages/form/src/fields/FormSelectDropdown.js
@@ -7,6 +7,8 @@ import _ from 'lodash';
 
 import SelectDropdown from '@hawk-ui/select-dropdown';
 
+import suggestAPI from '../utils/suggestAPI';
+
 export default class FormSelectDropdown extends Component {
   static propTypes = {
     value: PropTypes.oneOfType([
@@ -22,18 +24,53 @@ export default class FormSelectDropdown extends Component {
   };
   state = {
     query: '',
+    isLoading: false,
+    suggestions: [],
   };
   componentDidMount() {
     const { value, onChange } = this.props;
 
     onChange({ value });
   }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.debouncedSearch(this.state.query);
+    }
+  }
+  onSearchChange = (query) => {
+    if (_.size(query || '') > 0) {
+      const { visual } = this.props;
+
+      const suggest = _.get(visual, 'suggest', null);
+
+      if (!_.isEmpty(suggest) && !_.isEmpty(_.get(suggest, 'api'))) {
+        const data = _.get(suggest, 'data', 'data');
+        const queryParam = _.get(suggest, 'query', 'query');
+        const params = { [queryParam]: query };
+
+        this.setState({ isLoading: true });
+
+        suggestAPI(suggest.api, { params }).then((response) => {
+          this.setState({ isLoading: false, suggestions: response[data] });
+        }).catch(() => {
+          this.setState({ isLoading: false, suggestions: [] });
+        });
+      } else if (!_.isEmpty(suggest) && !_.isEmpty(_.get(suggest, 'options'))) {
+        const suggestOptions = _.get(suggest, 'options', []);
+
+        this.setState({ isLoading: false, suggestions: suggestOptions });
+      }
+    }
+  };
+  debouncedSearch = _.debounce(this.onSearchChange, 500);
 
   render() {
     const { property, noTitle, visual, onChange, value } = this.props;
+    const { isLoading, suggestions } = this.state;
     const options = _.get(visual, 'suggest.options', []);
     const renderOption = _.get(visual, 'suggest.name', 'title');
     const isIcon = _.get(visual, 'show_icon', false);
+    const isReadable = _.get(visual, 'is_readable', true);
 
     return (
       <div
@@ -43,8 +80,10 @@ export default class FormSelectDropdown extends Component {
         })}
       >
         <SelectDropdown
-          suggestions={options}
+          suggestions={!_.isEmpty(_.get(visual, 'suggest.options')) ? options : suggestions}
           isIcon={isIcon}
+          isSearchLoading={isLoading}
+          isReadOnly={isReadable}
           placeholder="Select anyone"
           renderSuggestion={(suggestion) => suggestion[renderOption]}
           searchValue={value}
