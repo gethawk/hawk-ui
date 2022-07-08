90f3e68e58604d0118a07b30871eaba5dc70bed7
Saurabh Gaur <saurabh@zenequity.com>
2022-07-08 12:49:16 +0530

    add toast overflow

--- packages/toast/src/index.js
+++ packages/toast/src/index.js
@@ -1,5 +1,5 @@
 // vendor modules
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import getClassnames from 'classnames';
 import _ from 'lodash';
@@ -19,6 +19,7 @@ export default class Toast extends Component {
       PropTypes.object,
     ]),
     isShow: PropTypes.bool,
+    isOverflow: PropTypes.bool,
     hideCloseOption: PropTypes.bool,
     isIcon: PropTypes.bool,
     icon: PropTypes.string,
@@ -30,47 +31,53 @@ export default class Toast extends Component {
     isIcon: false,
     type: 'success',
     position: 'bottom-left',
+    isOverflow: false,
   }
   state = {};
 
   render() {
-    const { position, type, icon, isIcon, isShow, children, hideCloseOption, onClick, onClose } = this.props;
+    const { position, type, icon, isIcon, isShow, isOverflow, children, hideCloseOption, onClick, onClose } = this.props;
 
     if (isShow) {
       return (
-        <div
-          className={getClassnames('hawk-toast', {
-            'hawk-toast__visible': isShow,
-            [`hawk-toast__${position}`]: _.isString(position),
-            [`hawk-toast__${position}-animation`]: _.isString(position),
-          })}
-          onClick={onClick}
-        >
-          <div className="hawk-toast__container">
-            <div className="hawk-toast__content">
-              {isIcon && (
-                <i
-                  className={getClassnames('fa hawk-toast__container-icon', {
-                    'fa-check-circle': _.isEqual(type, 'success'),
-                    'fa-times-circle': _.isEqual(type, 'failed'),
-                    'fa-info-circle': _.isEqual(type, 'info'),
-                    'fa-exclamation-triangle': _.isEqual(type, 'warning'),
-                    [icon]: _.isString(icon),
-                  })}
-                />
+        <Fragment>
+          <div
+            className={getClassnames('hawk-toast', {
+              'hawk-toast__visible': isShow,
+              [`hawk-toast__${position}`]: _.isString(position),
+              [`hawk-toast__${position}-animation`]: _.isString(position),
+            })}
+            onClick={onClick}
+          >
+            <div className="hawk-toast__container">
+              <div className="hawk-toast__content">
+                {isIcon && (
+                  <i
+                    className={getClassnames('fa hawk-toast__container-icon', {
+                      'fa-check-circle': _.isEqual(type, 'success'),
+                      'fa-times-circle': _.isEqual(type, 'failed'),
+                      'fa-info-circle': _.isEqual(type, 'info'),
+                      'fa-exclamation-triangle': _.isEqual(type, 'warning'),
+                      [icon]: _.isString(icon),
+                    })}
+                  />
+                )}
+                {children}
+              </div>
+              {hideCloseOption && (
+                <span
+                  className="hawk-toast__container-close"
+                  onClick={onClose}
+                >
+                  &times;
+                </span>
               )}
-              {children}
             </div>
-            {hideCloseOption && (
-              <span
-                className="hawk-toast__container-close"
-                onClick={onClose}
-              >
-                &times;
-              </span>
-            )}
           </div>
-        </div>
+          {isOverflow && (
+            <div className="hawk-toast__overflow"></div>
+          )}
+        </Fragment>
       );
     }
 
