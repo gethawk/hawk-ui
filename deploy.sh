d718d71956ac60cbae1e76cc4ca5dd5a4e82d55b
Saurabh Gaur <saurabh@zenequity.com>
2022-08-11 22:07:21 +0530

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set +x
+
+case "$1" in
+
+"prod")
+	export AWS_DEFAULT_REGION='ap-south-1'
+	export S3_BUCKET='hawk.wallnit.com'
+;;
+
+esac
+
+yarn run styleguide:build
+
+aws s3 cp index.html s3://${S3_BUCKET}/
+aws s3 cp --recursive ./build/ s3://${S3_BUCKET}/build
